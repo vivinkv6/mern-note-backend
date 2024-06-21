9164cd230508759d548ee23fe715e56217395563
vivinkv6 <vivinkv0@gmail.com>
2024-06-21 10:44:49 +0530

    fix new issues

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ const registerUser = async (req, res) => {
     const user = new User({ name, email, password });
 
     await user.save();
-    const token = jwt.sign(user.id, process.env.JWT_SECRET,{expiresIn:'2h'});
+    const token = jwt.sign(user.id, process.env.JWT_SECRET);
     res.status(201).json({ token, user: { id: user.id, name, email } });
   } catch (error) {
     res.status(500).json({ message: error});
@@ -25,15 +25,15 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid Credentials" });
+      return res.status(400).json({ message: "Wrong Email" });
     }
     //check if the password is correct or not
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid Credentials" });
+      return res.status(400).json({ message: "Wrong Password" });
     }
 
-    const token = jwt.sign(user.id, process.env.JWT_SECRET,{expiresIn:'2h'});
+    const token = jwt.sign(user.id, process.env.JWT_SECRET);
     res.status(200).json({ token, user: { id: user.id, name:user?.name, email } });
   } catch (error) {
     res.status(500).json({ message: error.message });
